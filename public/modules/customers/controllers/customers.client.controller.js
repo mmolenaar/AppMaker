a374e900f7944ebab2ada7175cd88865d1bf25cc
Lucas Molenaar <lucas@molenaarweb.nl>
2015-01-21 11:52:01 +0100

    Push to Azure

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -10,6 +10,33 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
 		// Find a list of Customers
 		this.customers = Customers.query();
 
+        // Open a modal window to create a single customer record
+        this.modalCreate = function (size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modules/customers/views/create-customer.client.view.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.ok = function () {
+                        if ($modalInstance.createCustomerForm.$valid) {
+                            $modalInstance.close();
+                        }
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: size
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+
 		// Open a modal window to Update a single customer record
 		this.modalUpdate = function (size, selectedCustomer) {
 
@@ -17,6 +44,16 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
 				templateUrl: 'modules/customers/views/edit-customer.client.view.html',
 				controller: function ($scope, $modalInstance, customer) {
 					$scope.customer = customer;
+
+					$scope.ok = function () {
+						if ($modalInstance.updateCustomerForm.$valid) {
+							$modalInstance.close($scope.customer);
+						}
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
 				},
 				size: size,
 				resolve: {
@@ -38,14 +75,70 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
 customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
 	function($scope, Customers) {
 
+        // Create new Customer
+        this.create = function() {
+        	// Create new Customer object
+        	var customer = new Customers ({
+        		firstname: this.firstname,
+        		surname: this.surname,
+        		suburb: this.suburb,
+        		country: this.country,
+        		industry: this.industry,
+        		email: this.email,
+        		phone: this.phone,
+        		referred: this.referred,
+        		channel: this.channel
+        	});
+
+            //Redirect after save
+            customer.$save(function(response) {
+
+                // Clear form fields
+                $scope.firstname = '';
+                $scope.surname = '';
+                $scope.suburb = '';
+                $scope.country = '';
+                $scope.industry = '';
+                $scope.email = '';
+                $scope.phone = '';
+                $scope.referred= '';
+                $scope.channel = '';
+
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 	}
 ]);
 
 customersApp.controller('CustomersUpdateController', ['$scope', 'Customers',
 	function($scope, Customers) {
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
 
+			customer.$update(function() {
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 	}
 ]);
+
+customersApp.directive('customerList', [function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'modules/customers/views/customer-list-template.html',
+        link: function(scope, element, attrs){
+
+        }
+    };
+}]);
+
+
+
 		//// Create new Customer
 		//$scope.create = function() {
 		//	// Create new Customer object
